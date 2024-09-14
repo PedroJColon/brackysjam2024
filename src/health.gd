0c59c7493aa4b2cdb3c7d80b1a0c51cf35b4b6e2
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2024-09-14 17:41:56 -0400

    Add additional functionality to Health

--- src/health.gd
+++ src/health.gd
@@ -2,18 +2,16 @@ extends Node
 
 @export var health = 3
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+var dead = false
 
 func decrement_health(_decrease: float) -> void:
-	if health <= 0:
+	if check_health():
 		health = 0
+		dead = true
 	health -= _decrease
 	
+func check_health() -> bool:
+	return health <= 0
+
+func check_state() -> bool:
+	return dead
